a18038585cdcc9f569bf5e90fa16e9214cf570aa
Roxiq Mulna Muhammad <roxiqmm@gmail.com>
2021-12-16 11:07:15 +0000

    muncul error saat mengerjakan tugas 14 karena pada file mahasiswa seharusnya huruf M-nya kecil import Mahasiswa from "./Tugas-13/mahasiswa";

--- tugas-harian-part-2/src/App.js
+++ tugas-harian-part-2/src/App.js
@@ -3,7 +3,7 @@ import Tugas12 from "./Tugas-12/tugas12";
 import Tugas11 from "./Tugas-11/tugas11";
 import Tugas10 from "./Tugas-10/tugas10";
 import Tugas9 from "./Tugas-9/tugas9";
-import Mahasiswa from "./Tugas-13/Mahasiswa";
+import Mahasiswa from "./Tugas-13/mahasiswa";
 
 
 
